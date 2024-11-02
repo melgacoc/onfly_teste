63e7914ff0227e6792d939b0ded17610d1446373
Claudio <claudio19a@hotmail.com>
2024-11-02 12:49:17 -0300

    Rotas para criação de novo usuario e login

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'user' => [
+                'name' => $user->name,
+                'email' => $user->email,
+                'token' => $token,
+            ],
+            'status' => 'success',
+            'message' => 'User created successfully',
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => [
+                'name' => $user->name,
+                'email' => $user->email,
+                'token' => $token,
+            ],
+            'status' => 'success',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return response()->json([
@@ -9,6 +10,9 @@ Route::get('/user', function (Request $request) {
         'message' => 'User data fetched successfully',
     ], 200);
 });
+
+Route::post('/newUser', [App\Http\Controllers\UserController::class, 'store']);
+Route::post('/login', [App\Http\Controllers\UserController::class, 'login']);
 
 //Route::get('/user', function (Request $request) {
     //return $request->user();
